bc2b920dfaa37a35c3443c7dc29a42755b5eee41
Andremelo001 <andre.bezerra2@aluno.ce.gov.br>
2025-02-23 09:42:52 -0300

    feature: creat crud client

--- app/main.py
+++ app/main.py
@@ -1,10 +1,7 @@
 from fastapi import FastAPI
-from routes import ClientRoutes, PetRoutes, ScheduleRoutes, ServicesRoutes
+from routes import ClientRoutes
 
 app = FastAPI()
 
 # Rotas para Endpoints
 app.include_router(ClientRoutes.router)
-app.include_router(PetRoutes.router)
-app.include_router(ScheduleRoutes.router)
-app.include_router(ServicesRoutes.router)
